df88ad55cf7a69bdb8a073a83b23b3c401812b54
Aiden <xtsonightmare@gmail.com>
2021-11-19 20:30:22 -0500

    adding scss

---
+++ src/screens/homeScreen.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Box } from "@mui/system"
+
+
+
+const HomeScreen = () => {
+    return ( 
+        <div >
+            <h1> Hellow Box </h1>  
+        </div >
+    );
+}
+
+export default HomeScreen;
