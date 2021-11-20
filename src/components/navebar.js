8dd13caef46a822c9166731604fc96e35352935e
Aiden <xtsonightmare@gmail.com>
2021-11-19 20:43:40 -0500

    importing mui icons

--- src/components/navebar.js
+++ src/components/navebar.js
@@ -1,8 +1,10 @@
 import React from "react"
+import HomeIcon from '@mui/icons-material/Home';
 
 export const NavBar = () => {
     return ( 
         <div >
+            <HomeIcon/>
             <h1> Hellow Box </h1>  
         </div >
     );
--- src/container/homeScreen.js
+++ src/container/homeScreen.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Box } from "@mui/system"
 import { NavBar } from "../components/navebar"
 
 const HomeScreen = () => {
