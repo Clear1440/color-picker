1574e904eeba081f69430988d11f918b2717923c
Aiden <xtsonightmare@gmail.com>
2021-12-05 22:23:22 -0500

    got it to work!! serializations amiritie :)

--- src/container/homescreen/index.js
+++ src/container/homescreen/index.js
@@ -1,3 +1,4 @@
+/*global chrome*/
 import React, {useState} from "react"
 //MUI
 import List from '@mui/material/List';
@@ -10,47 +11,76 @@ import NavBar from "./home-navbar";
 //STYLES
 import "../../stylesheets/layout/page.scss"
 import {useNavigate} from "react-router-dom";
-import {changeColor, unsetChangeColor} from "../../scripts/content";
 
 const HomeScreen = () => {
     let navigate = useNavigate();
     const [items,setItems] = useState([
-            {name:'tan header',avatar:'example',id:1, style: [
-                    {id: "site-nav", style: "background-color: tan;"},
-                    {id: "site-nav-desktop-item", style: "background-color: black"},
-                    {id: "filter-subnav", style: "background-color: darkgrey"},
-                    {id: "wrap-inner", style: "background-color: darkgrey"}
-                ]},
-            {name:'grey background',avatar:'example',id:2, style: [
-                {id: "site-nav-desktop-item", style: "background-color: black"},
-                    {id: "filter-subnav", style: "background-color: darkgrey"},
-                    {id: "wrap-inner", style: "background-color: darkgrey"}]},
-            {name:'pink',avatar:'example',id:3, style: [
-                    {id: "site-nav", style: "background-color: pink;"},
-                    {id: "site-nav-desktop-item", style: "background-color: pink"},
-                    {id: "filter-subnav", style: "background-color: pink"},
-                    {id: "wrap-inner", style: "background-color: pink"}]
+            {
+                name:'tan header',
+                avatar:'example',
+                id:1, 
+                style: `.site-nav { 
+                    background-color: tan
+                }`
+            },{
+                name:'grey header',
+                avatar:'example',
+                id:2,
+                style: 
+                    `.site-nav {
+                        background-color: grey
+                    }`
+            },{
+                name:'pink header',
+                avatar:'example',
+                id:3, 
+                style: 
+                    `.site-nav {
+                        background-color: pink
+                    }`
             },
     ]);
 
-    const [selectedItem, setSelectedItem] = useState({name:'default',avatar:'example',id:7, style: [{id: "page", style: "none"}]});
+    const [selectedItem, setSelectedItem] = useState({
+        name:'default',
+        avatar:'example',
+        id:7, 
+        style: ""
+    });
 
-    const clickHandler = async id => {
+    const addCustomStyling = (customCSS) => {
+        const styleFunc = (css) => {
+            //remove old styling changes
+            document.getElementById("custom-styling-picker")?.remove();
+            //add new stylings
+            const styleScript = document.createElement('style');
+            styleScript.id = "custom-styling-picker";
+            styleScript.innerHTML = css;
+            document.head.appendChild(styleScript);
+        }
+
+        //put ^ in the webpage
+        chrome.tabs.executeScript({
+            code: `(${styleFunc})('${customCSS.replace(/\n/g, ' ')}');`
+        }, (e) => {
+            console.log('Popup script error:', e)
+        });
+    }
+
+
+    const clickHandler = id => {
         const _selectedItem = selectedItem;
         const _items = [...items];
         const index = items.findIndex(item => item.id === id);
-        //remove color styling
-        unsetChangeColor(_selectedItem.style);
         //Set selected item
         setSelectedItem(items[index]);
         _items.splice(index, 1);
         _items.push(_selectedItem);
         setItems(_items.sort((a,b) =>  a.name.localeCompare(b.name)));
-        //apply new color styling
-        changeColor(items[index].style);
+        addCustomStyling(items[index].style);
     }
 
-    const clickHandlerSelected = id => {
+    const clickHandlerSelected = () => {
         navigate('/web-theme');
     }
 
